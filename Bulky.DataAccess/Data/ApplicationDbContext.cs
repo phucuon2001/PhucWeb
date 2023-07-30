188ea3b6442530c5c8b56ba518ef9034360f6f22
phucuon2001 <136848346+phucuon2001@users.noreply.github.com>
2023-07-31 04:56:49 +0700

    Add project files.

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAcess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser> 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 }
+              
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Fortune of Time",
+                    Author = "Billy Spark",
+                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+                    ISBN = "SWD9999001",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80,
+                    CategoryId = 1,
+                    ImageUrl = ""
+                }           
+                );
+        }
+    }
+}
